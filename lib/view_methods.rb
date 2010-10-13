bd1f96627b87625f671a60a22dfc239b0e34c0e5
Ebony <ebonical@gmail.com>
2010-10-13 21:30:57 +0100

    change binding to view

--- lib/view_methods.rb
+++ lib/view_methods.rb
@@ -3,20 +3,20 @@ module SimpleMenus
     class Menu
       attr_accessor :id
       
-      def initialize(binding, *args)
-        @binding = binding
+      def initialize(view, *args)
+        @view = view
         @options = args.extract_options!.reverse_merge(:class => "")
         self.id = args.first
         @options[:id] = id.to_s if id
         @options[:class] << ' simple_menu'
         @items = []
-        @current_items = @binding.instance_variable_get('@simple_menu_current_items')[id] rescue []
+        @current_items = @view.instance_variable_get('@simple_menu_current_items')[id] rescue []
       end
       
       def method_missing(item_id, *args, &block)
         options = args.extract_options!
-        item = MenuItem.new(@binding, item_id, options)
-        item.content = block_given? ? @binding.capture(&block) : args.first
+        item = MenuItem.new(@view, item_id, options)
+        item.content = block_given? ? @view.capture(&block) : args.first
         item.current = @current_items.include?(item.id)
         item.first = @items.empty?
         @items << item
@@ -25,7 +25,7 @@ module SimpleMenus
       
       def to_s
         @items.last.last = true unless @items.empty?
-        tag_options = @binding.send :tag_options, @options
+        tag_options = @view.send :tag_options, @options
         "<div #{tag_options}><ul>#{@items}</ul></div>"
       end
     end
@@ -33,8 +33,8 @@ module SimpleMenus
     class MenuItem
       attr_accessor :first, :last, :id, :current, :content
       
-      def initialize(binding, item_id, options)
-        @binding = binding
+      def initialize(view, item_id, options)
+        @view = view
         self.id = item_id
         @options = options
         @classes = (@options[:class] || '').split(' ')
@@ -45,7 +45,7 @@ module SimpleMenus
         @classes << 'last' if last
         @classes << 'current' if current
         @options[:class] = @classes.join(' ')
-        tag_options = @binding.send :tag_options, @options
+        tag_options = @view.send :tag_options, @options
         "<li #{tag_options}>#{content}</li>"
       end
     end
